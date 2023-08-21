0fbd974a22a12dd471e221186d17fa650254ed95
regruss <44583658+regruss@users.noreply.github.com>
2023-08-21 14:15:17 -0500

    Add files via upload

--- 01_Home.py
+++ 01_Home.py
@@ -34,8 +34,8 @@ def main():
     if bool(pdf_files):
         st.header('Contract Data Preview')
         # Insert into DF
-        contract_df = pd.read_csv(r'Contract_Data_Demo\Contract_Portfolio_DF.csv')
-        budget_df = pd.read_csv(r'Contract_Data_Demo\Budget_Portfolio_DF.csv')
+        contract_df = pd.read_csv(r'https://github.com/regruss/Contract_Data_Demo/Contract_Portfolio_DF.csv')
+        budget_df = pd.read_csv(r'https://github.com/regruss/Contract_Data_Demo/Budget_Portfolio_DF.csv')
         st.write(contract_df.head())
         st.header('Budget Data Preview')
         st.write(budget_df.head())
