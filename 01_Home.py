9e88ee336cd39a91d53f6970b124c9dfee621fb5
regruss <44583658+regruss@users.noreply.github.com>
2023-08-22 11:08:05 -0500

    Add files via upload

--- 01_Home.py
+++ 01_Home.py
@@ -9,7 +9,6 @@ import os
 import json
 import numpy as np
 import pandas as pd
-# import requests
 import time
 import spacy
 import openai
@@ -35,9 +34,8 @@ def main():
     if bool(pdf_files):
         st.header('Contract Data Preview')
         # Insert into DF
-        # contract_request = request.get(r'https://github.com/regruss/Contract_Data_Demo/Contract_Portfolio_DF.csv')
-        # contract_df = pd.read_csv(contract_request)
-        budget_df = pd.read_csv(r'Budget_Portfolio_DF.csv')
+        contract_df = pd.read_csv(r'C:\Users\regru\Desktop\PDF_OCR\NegotiationAI_Example_Data\Sample_Data_Generation\Contract_Portfolio_DF.csv')
+        budget_df = pd.read_csv(r'C:\Users\regru\Desktop\PDF_OCR\NegotiationAI_Example_Data\Sample_Data_Generation\Budget_Portfolio_DF.csv')
         st.write(contract_df.head())
         st.header('Budget Data Preview')
         st.write(budget_df.head())
